09ec3ce75c991d0193881defb7b0998f228d217e
Wenhong Sun <whsun@ucla.edu>
2025-02-04 10:07:21 -0800

    Updated multiple pages: index, coursework, personal, academic, projects, and added analytics tracking

---
+++ analytics.js
@@ -0,0 +1,6 @@
+// analytics.js - Google Analytics Tracking Code
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-FSEQT53CPJ');
